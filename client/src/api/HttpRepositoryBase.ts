49cbcef5e18d436e0b76f73bb8fa87794bdd284b
Recep ŞEN <recepsen@taptoweb.com>
2022-12-11 21:56:02 +0300

    feat: create httpRepositoryBase class

---
+++ client/src/api/HttpRepositoryBase.ts
@@ -0,0 +1,16 @@
+import { AxiosResponse } from "axios";
+
+export class HttpRepositoryBase {
+  async send(
+    request: Promise<AxiosResponse<any, any>>,
+    callback: ((data: any) => void) | undefined = undefined
+  ) {
+    return request
+      .then((response: { data: any }) => response.data)
+      .then((data) => {
+        if (callback) callback(data);
+        return data;
+      })
+      .catch((error) => error);
+  }
+}
