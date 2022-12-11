06df6b490be89b4c1bac25d4b7ab8f330856d3dd
Recep ŞEN <recepsen@taptoweb.com>
2022-12-11 21:52:58 +0300

    fix: errorHandler middleware response error

--- server/src/middlewares/errorHandler.middleware.js
+++ server/src/middlewares/errorHandler.middleware.js
@@ -5,8 +5,8 @@ export const errorHandlerMiddleware = async (err, req, res, next) => {
   if (err) {
     const status = err.status || HttpStatusCodes.INTERNAL_SERVER_ERROR;
     const path = err.path || "global";
-    console.error(err);
-    res.json(ServiceResponse.fail(status, true, path, [err.message]));
+    // console.error(err);
+    return res.json(ServiceResponse.fail(status, true, path, [err.message]));
   }
   next();
 };
