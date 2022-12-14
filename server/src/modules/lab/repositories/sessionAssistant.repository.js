efaf94ce5a12afa29e8b276d5510ee8cbf09b8b7
hiikmetgezmen <hikmetgezmen.hg@gmail.com>
2022-12-14 18:16:42 +0300

    refac: Fix methods

--- server/src/modules/lab/repositories/sessionAssistant.repository.js
+++ server/src/modules/lab/repositories/sessionAssistant.repository.js
@@ -1,15 +1,8 @@
-import Session from "../models/Session.schema.js";
-import sessionAssistant from "../models/SessionAssistant.schema.js";
+import SessionAssistant from "../models/SessionAssistant.schema.js";
 
 class sessionAssistantRepository {
-  async createAssistant(userId) {
-    const saved = await sessionAssistant.create({
-      user: userId,
-    });
-    return saved;
-  }
   getAll(userId) {
-    return sessionAssistant.find({ user: userId }).populate("user");
+    return SessionAssistant.find({ user: userId }).populate("user");
   }
   getById(id) {
     return User.findById(id);
