7342f534c5dc5d3ff6291c08610005232b7dc772
hiikmetgezmen <hikmetgezmen.hg@gmail.com>
2022-12-17 14:47:31 +0300

    feat: Add get sections and update exit time

--- server/src/modules/lab/repositories/userSessions.repository.js
+++ server/src/modules/lab/repositories/userSessions.repository.js
@@ -5,8 +5,8 @@ import HttpStatusCodes from "http-status-codes";
 
 class userSessionRepository {
   async createUserSession(userId) {
-    const findSessionDate = await this.ifExistDate(new Date());
-    if (!findSessionDate)
+    const findSessionDate = await this.getDate(new Date());
+    if (!findSessionDate || findSessionDate.closeTime != null)
       throw new ApiError(
         "Session not found",
         HttpStatusCodes.BAD_REQUEST,
@@ -28,24 +28,64 @@ class userSessionRepository {
     );
     return userSession;
   }
-  ifExistDate(date) {
-    return this.getByDate(date);
-  }
   getByDate(date) {
     const querry = {
-      day: date.getDate(),
+      day: date.getDate() + 1,
       year: date.getFullYear(),
-      month: date.getMonth() + 1,
+      month: date.getMonth(),
     };
     return Session.findOne({
-      date: {
+      openTime: {
         $gte: new Date(querry.year, querry.month, querry.day),
         $lt: new Date(querry.year, querry.month, querry.day + 1),
       },
     });
   }
-  getAllUserSessions() {
-    return UserSession.find({});
+  getDate() {
+    const found = this.getByDate(new Date());
+    if (!found)
+      throw new ApiError(
+        "Session not found",
+        HttpStatusCodes.BAD_REQUEST,
+        "userSessionRepository->getBy"
+      );
+    return found;
+  }
+  async addExitTime(userId) {
+    const found = await this.getDate();
+    const updateTime = await UserSession.findOneAndUpdate(
+      {
+        session: found._id,
+        userId: userId,
+        exitTime: null,
+      },
+      {
+        exitTime: Date.now(),
+      },
+      { new: true, useFindAndModify: false }
+    );
+    return updateTime;
+  }
+  async getAllUserSessionsByDate() {
+    const found = await this.getDate(new Date());
+    const getUser = await UserSession.find({ session: found._id });
+    return getUser;
+  }
+  async getAllUserSessions() {
+    const getUser = await UserSession.find({});
+    return getUser;
+  }
+  async getAllUsersInLab() {
+    const found = await this.getDate(new Date());
+    const getUser = await UserSession.find({
+      session: found._id,
+      exitTime: null,
+    });
+    return getUser;
+  }
+  async numberOfUsersInLab(){
+    const number = (await this.getAllUsersInLab()).length;
+    return number;
   }
 }
 
--- server/src/modules/lab/services/userSession.service.js
+++ server/src/modules/lab/services/userSession.service.js
@@ -1,13 +1,29 @@
 import userSessionRepository from "../repositories/userSessions.repository.js";
 
 class userSessionService {
-  async getAllUserSessions() {
-    const session = await userSessionRepository.getAllUserSessions();
+  async getAllUserSessions(id) {
+    const session = await userSessionRepository.getAllUserSessions(id);
     return session;
   }
-  async createUserSession(session) {
-    const created = await userSessionRepository.createUserSession(session);
+  async getAllUserSessionsByDate(id) {
+    const session = await userSessionRepository.getAllUserSessionsByDate(id);
+    return session;
+  }
+  async getAllUsersInLab(id) {
+    const session = await userSessionRepository.getAllUsersInLab(id);
+    return session;
+  }
+  async createUserSession(userId) {
+    const created = await userSessionRepository.createUserSession(userId);
     return created;
+  }
+  async addExitTime(date) {
+    const created = await userSessionRepository.addExitTime(date);
+    return created;
+  }
+  async numberOfUsersInLab(id) {
+    const session = await userSessionRepository.numberOfUsersInLab(id);
+    return session;
   }
 }
 
