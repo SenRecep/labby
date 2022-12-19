5b246bfa3861e8322486bc739f3931bef129488f
hiikmetgezmen <hikmetgezmen.hg@gmail.com>
2022-12-19 14:23:28 +0300

    feat: Create userSession route

---
+++ server/src/modules/lab/routers/userSession.route.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { requiredAuthMiddleware } from "../../../middlewares/auth.middleware.js";
+import { postUserSessionRequest , getAllUserSessions,postExitTimeRequest, getAllUsersInLab, numberOfUsersInLab, getAllUserSessionsByDate} from "../controllers/userSession.controller.js";
+const router = express.Router();
+
+
+  router
+  .route("/")
+  .get(requiredAuthMiddleware,getAllUserSessionsByDate)
+  .post(postUserSessionRequest)
+  .put(postExitTimeRequest);
+
+  router
+  .route("/all")
+  .get(requiredAuthMiddleware,getAllUserSessions);
+
+  router
+  .route("/inlab")
+  .get(requiredAuthMiddleware,getAllUsersInLab);
+
+  router
+  .route("/number")
+  .get(requiredAuthMiddleware,numberOfUsersInLab);
+  
+  export default router;
+  
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,11 +2,12 @@ import authRoute from "../modules/auth/routers/auth.route.js";
 import userRoute from "../modules/auth/routers/user.route.js";
 import adminRoute from "../modules/admin/routers/admin.route.js";
 import sessionRoute from "../modules/lab/routers/session.route.js";
+import usersessionRoute from "../modules/lab/routers/userSession.route.js";
 
 export const useRoutes = (app) => {
   app.use("/api/auth", authRoute);
   app.use("/api/users", userRoute);
   app.use("/api/admin", adminRoute);
-  app.use("/api/session",sessionRoute)
-
+  app.use("/api/session",sessionRoute);
+  app.use("/api/usersession",usersessionRoute);
 };
