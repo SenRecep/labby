a814aae59e3c243cc774e4aaeee56b851b907f62
yusuftopkaya <yusuftopkaya@protonmail.com>
2022-12-25 13:51:32 +0300

    feat: Html mail templates

--- server/src/modules/email/transactions/forgotpass.js
+++ server/src/modules/email/transactions/forgotpass.js
@@ -1,11 +1,12 @@
 import EmailService from "../services/email.service.js";
 import verificationCodeRepository from "../repositories/verificationCode.repository.js";
 import userRepository from "../../auth/repositories/user.repository.js";
-import { ApiError } from "../../../common/apiError.js";
+import handlebars from "handlebars";
 
 export const sendVerificationCode=async(email)=>{
    const verificationCode= await verificationCodeRepository.CreateCode(email);
-   EmailService.SendMail(email,"Renew your password!",verificationCode.code);
+   const htmlTemplate=createHtmlTemplate(verificationCode.code); 
+   EmailService.SendMailWithTemplates(email,'Renew your password!',htmlTemplate);
 }
 
 export const verifyCode=async(email,code)=>{
@@ -18,3 +19,16 @@ export const changePassword=async(email,code,password)=>{
     const user=await userRepository.getByEmail(email);
     userRepository.updatePassword(user.id,password);
 }
+
+const createHtmlTemplate=(code)=>{
+    const filePath = path.join(__dirname, '../templates/forgotPassword.html');
+    const source = fs.readFileSync(filePath, 'utf-8').toString();
+    const template = handlebars.compile(source);
+    const replacements = {
+      code: code
+    };
+    const htmlToSend = template(replacements);
+    return htmlToSend;
+}
+
+export default {sendVerificationCode,verifyCode,changePassword};
