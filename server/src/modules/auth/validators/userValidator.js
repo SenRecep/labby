d55d0ee0bbc75400622d7b993bfa5ff05be674c8
hiikmetgezmen <hikmetgezmen.hg@gmail.com>
2023-01-02 13:34:41 +0300

    refac: Fix student number valid

--- server/src/modules/auth/validators/userValidator.js
+++ server/src/modules/auth/validators/userValidator.js
@@ -8,5 +8,6 @@ export const userValidator = [
     .normalizeEmail()
     .toLowerCase(),
   body("password").trim().isLength(2).withMessage("Password is too short"),
-  body("phone").trim().isLength(10).isMobilePhone("tr-TR").withMessage("Invalid phone number.")
+  body("studentNumber").trim().isLength({ min: 9, max:9}).withMessage("Invalid student number."),
+  body("phone").trim().isLength(10).isMobilePhone("tr-TR").withMessage("Invalid phone number."),
 ];
