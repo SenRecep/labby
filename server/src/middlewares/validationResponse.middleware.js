e465fd42796162a77913ef7c864507ebb6f87e05
Recep ŞEN <recepsen@taptoweb.com>
2023-01-02 13:48:26 +0300

    refactor: ValidationResponseMiddleware errors

--- server/src/middlewares/validationResponse.middleware.js
+++ server/src/middlewares/validationResponse.middleware.js
@@ -5,16 +5,14 @@ import HttpStatusCodes from "http-status-codes";
 export const validationResponseMiddleware = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(HttpStatusCodes.BAD_REQUEST)
-      .json(
-        ServiceResponse.fail(
-          HttpStatusCodes.BAD_REQUEST,
-          true,
-          "validationResponseMiddleware",
-          errors.array()
-        )
-      );
+    return res.status(HttpStatusCodes.BAD_REQUEST).json(
+      ServiceResponse.fail(
+        HttpStatusCodes.BAD_REQUEST,
+        true,
+        "validationResponseMiddleware",
+        errors.array().map((x) => x.msg)
+      )
+    );
   }
   return next();
 };
